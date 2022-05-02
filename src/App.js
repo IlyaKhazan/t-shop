8edc792209eba61ed1d3a194fcd40cf53a0c8d1f
Ilya Khazan <p9067700518@gmail.com>
2022-05-02 13:53:15 +0300

    Свёрстаны основные блоки

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <div className="logoWrapper">
+          <img width={40} height={40} src="img/logo.png" alt="Логотип магазина" />
+          <div className="logoInfo">
+            <h2>T-Shop</h2>
+            <p>Лучший магазин</p>
+          </div>
+        </div>
+
+        <ul className="infoRight">
+          <li>
+            <img width={20} height={20} src="img/icons/cart.svg" alt="Корзина" />
+            <span className="totalCost">1500р</span>
+          </li>
+          <li>
+            <img width={20} height={20} src="img/icons/user.svg" alt="Личный кабинет" />
+          </li>
+          <li>
+            <img width={20} height={20} src="img/icons/like.svg" alt="Избранное" />
+          </li>
+        </ul>
       </header>
+
+      <div className="contentWrapper">
+        <h1>Все товары</h1>
+        <div className="cardWrapper">
+          <div className="card">
+            <img width={135} height={120} src="img/items/1.jpg" alt="Фото товара" />
+            <p className="itemTitle">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div className="infoWrapper">
+              <div className="priceWrapper">
+                <p className="priceTitle">Цена:</p>
+                <p className="priceValue">11 000р</p>
+              </div>
+              <button className="button">
+                <img width={10} height={10} src="img/icons/plus.svg" alt="В корзину" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={135} height={120} src="img/items/2.jpg" alt="Фото товара" />
+            <p className="itemTitle">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div className="infoWrapper">
+              <div className="priceWrapper">
+                <p className="priceTitle">Цена:</p>
+                <p className="priceValue">11 000р</p>
+              </div>
+              <button className="button">
+                <img width={10} height={10} src="img/icons/plus.svg" alt="В корзину" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={135} height={120} src="img/items/3.jpg" alt="Фото товара" />
+            <p className="itemTitle">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div className="infoWrapper">
+              <div className="priceWrapper">
+                <p className="priceTitle">Цена:</p>
+                <p className="priceValue">11 000р</p>
+              </div>
+              <button className="button">
+                <img width={10} height={10} src="img/icons/plus.svg" alt="В корзину" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={135} height={120} src="img/items/4.jpg" alt="Фото товара" />
+            <p className="itemTitle">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div className="infoWrapper">
+              <div className="priceWrapper">
+                <p className="priceTitle">Цена:</p>
+                <p className="priceValue">11 000р</p>
+              </div>
+              <button className="button">
+                <img width={10} height={10} src="img/icons/plus.svg" alt="В корзину" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
