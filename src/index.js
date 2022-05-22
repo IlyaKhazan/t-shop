59561810563ace0772e13f8c05a54a5400672af8
Ilya Khazan <p9067700518@gmail.com>
2022-05-22 16:33:47 +0300

    Добавлен react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.scss';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
